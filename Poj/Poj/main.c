22e28a1aace501480e89201330ff4cae53577edb
hechanghui <hechanghui@blockchainrdn.com>
2020-04-07 11:12:58 +0800

    poj 1001

---
+++ Poj/Poj/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Poj
+//
+//  Created by 至臻天龙 on 2020/4/7.
+//  Copyright © 2020 hech. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
